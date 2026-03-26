651faca980a66275bfdb56175d503d2c84b54638
dikshasharma4 <diksha1809sharma@gmail.com>
2026-03-26 22:22:58 +0530

    vercel config added

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.post("/send", async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-  console.log("🚀 Server running at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
